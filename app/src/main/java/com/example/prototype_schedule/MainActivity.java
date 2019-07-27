6d6bb959b66e1d091bed161e0d890beae56f50c0
Charlotte Wang <shallotwang129@gmail.com>
2019-07-26 21:40:05 -0400

    Set-UpScreen+Welcome

--- app/src/main/java/com/example/prototype_schedule/MainActivity.java
+++ app/src/main/java/com/example/prototype_schedule/MainActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -14,14 +17,36 @@ public class MainActivity extends AppCompatActivity {
     private ArrayList<String>Classes = new ArrayList<>();
     private ArrayList<String>Times = new ArrayList<>();
     private ArrayList<String>Periods = new ArrayList<>();
+    private LinearLayout welcome;
+    private boolean setUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setUp = false;
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        generateData();
-        setUpRecycler();
 
+        if(setUp == true){
+            //Change Later to LOAD data
+            setContentView(R.layout.activity_main);
+            generateData();
+            setUpRecycler();
+        }
+        else{
+            setContentView(R.layout.hello);
+            welcome = (LinearLayout) findViewById(R.id.click_hello);
+            welcome.setOnClickListener(new Hello_Listener());
+        }
+
+
+
+    }
+
+    private class Hello_Listener implements View.OnClickListener{
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(MainActivity.this, SetUp.class);
+            startActivity(intent);
+        }
     }
 
     private void generateData(){
---
+++ app/src/main/java/com/example/prototype_schedule/SetUp.java
@@ -0,0 +1,15 @@
+package com.example.prototype_schedule;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SetUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.setup_blue);
+    }
+}
